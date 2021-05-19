e242fd365c4dd554fff3ddf59aa63df2b27ab5e9
Philippe Roy <renaissance542@gmail.com>
2021-05-19 19:00:03 -0400

    created basic board with shaking functionality

---
+++ script.js
@@ -0,0 +1,54 @@
+const board = document.getElementById('gameBoard');
+document.getElementById('new-board').addEventListener('click', newBoard);
+document.getElementById('shake').addEventListener('click', shake);
+
+function newBoard() {
+    deleteBoard();
+    let size = prompt("How many squares?", 50);
+    // create number of rows equal to user input 'size'
+    for (let i = 0; i < size; i++){
+        let row = document.createElement("div");
+        row.classList.add('row');
+        populateCells(row, size);
+        board.appendChild(row);
+    };
+}
+
+function deleteBoard(){
+    while(board.firstChild){
+        board.removeChild(board.firstChild);
+    }
+}
+
+// create number of cells equal to user input 'size'
+function populateCells (row, size){
+    for(let i = 0; i<size; i++){
+        let cell = document.createElement("div");
+        cell.classList.add('cell');
+        
+        // let spacer = document.createElement('div');
+        // spacer.classList.add('spacer');
+        // cell.appendChild(spacer);
+
+        cell.addEventListener('mouseover', darkenCell);
+        row.appendChild(cell);
+    }
+}
+
+function darkenCell(event){
+    event.target.classList.add('darkened');
+}
+
+function shake(){
+    board.classList.add('shake');
+    setTimeout(clearCells, 1200);
+}
+
+function clearCells(){
+    let cells = document.getElementsByClassName('cell');
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].classList.remove('darkened');
+    }
+    board.classList.remove('shake');
+}
+
